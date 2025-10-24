3c4adc0e59c5be956bb3b44ab451583e15c676aa
Shirley Tarboton <shirley@tarboton.co.uk>
2025-10-24 17:53:01 +0100

    updated message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Kubernetes!"
+    return "Hello from Kubernetes with a new message!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
